0d3193f7598de805e2c56cf11111bd9e2d65dbaa
Nguyễn Long <hoanglong.lumi404@gmail.com>
2024-07-25 20:42:58 +0700

    greatlte: Setup retrofit dynamic partitions

--- device.mk
+++ device.mk
@@ -5,5 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+	fastbootd
+
 # Python prebuilts
 TW_EXCLUDE_PYTHON := true
